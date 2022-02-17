5af0ca493a223a5159f3ca0c58d0dee9806dcc6f
Juan Drago <juan.drago@patagonian.it>
2022-02-17 09:53:12 -0300

    Frontend: Basic Routing and Auth Screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { AuthScreen } from "./screens/Auth/AuthScreen";
+import { LoginScreen } from "./screens/Login/LoginScreen";
+import { RegisterScreen } from "./screens/Register/RegisterScreen";
 
 function App() {
-  return <AuthScreen />;
+  return (
+    <Routes>
+      <Route path="/" element={<AuthScreen />} />
+      <Route path="/signup" element={<RegisterScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/screens/Register/RegisterScreen.tsx
+++ frontend/src/screens/Register/RegisterScreen.tsx
@@ -2,15 +2,12 @@ import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import { Link as RouterLink } from "react-router-dom";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
-export const LoginScreen = () => {
+export const RegisterScreen = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -35,7 +32,7 @@ export const LoginScreen = () => {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Sign up
         </Typography>
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
           <TextField
@@ -52,11 +49,28 @@ export const LoginScreen = () => {
             margin="normal"
             required
             fullWidth
+            id="nickname"
+            label="Nickname"
+            name="nickname"
+            autoComplete="nickname"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
             name="password"
             label="Password"
             type="password"
             id="password"
-            autoComplete="current-password"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="repeatPassword"
+            label="Confirm Password"
+            type="repeatPassword"
+            id="repeatPassword"
           />
           <Button
             type="submit"
@@ -64,15 +78,8 @@ export const LoginScreen = () => {
             variant="contained"
             sx={{ mt: 3, mb: 2 }}
           >
-            Sign In
+            Sign Up
           </Button>
-          <Grid container>
-            <Grid item>
-              <RouterLink to="/signup">
-                <Link variant="body2">{"Don't have an account? Sign Up"}</Link>
-              </RouterLink>
-            </Grid>
-          </Grid>
         </Box>
       </Box>
     </Container>
