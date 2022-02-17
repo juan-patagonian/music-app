7eafcdc5588c8c7e93dc4fb5ccfde0e0fd799a41
Juan Drago <juan.drago@patagonian.it>
2022-02-17 10:45:19 -0300

    Common: Added cors rules and proxy

--- backend/app.ts
+++ backend/app.ts
@@ -2,9 +2,15 @@ import express from "express";
 import { connectToMongoDB } from "./dbconnection";
 import mainRouter from "./src/routes/main";
 import passport from "./src/services/auth/passport";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+var corsOptions = {
+  origin: "http://localhost:5000",
+};
+app.use(cors(corsOptions));
 
 const passportMiddleware = passport.initialize();
 
@@ -15,7 +21,7 @@ app.use(
     extended: true,
   })
 );
-app.use("/", mainRouter);
+app.use("/api", mainRouter);
 
 app.listen(port, async () => {
   console.log(`App running on port ${port}.`);
